92ffe3a0c4ad89a380ba0a5e4683ad24749df690
Solène Dietsch <solene.dietsch@gmail.com>
2020-06-10 16:58:55 +0200

    Implementation of the calculate compontents function that return estimated peak value(for now max) and half value

--- master-thesis-2020/data_analysis.py
+++ master-thesis-2020/data_analysis.py
@@ -7,6 +7,22 @@ Created on Mon Jun  8 16:20:26 2020
 
 import pandas as pd
 
+def calculate_components(dataframe,column1,column2,dv=0.1):
+    series1 = dataframe[column1]
+    series2 = dataframe[column2]
+    
+
+    value_at_peak = series2.max()
+    value_at_peak_idx = series2.idxmax()
+    value_at_half_life = value_at_peak/2
+
+    troncated_series2 = series2.loc[value_at_peak_idx:]
+    series2_btw = troncated_series2[troncated_series2.between(value_at_half_life - dv,value_at_half_life + dv)]
+    
+    half_life_value = series1.loc[abs(series2_btw - value_at_half_life).idxmin()]
+    #half_life_value = series1.loc[abs(series2_btw - value_at_half_life).idxmin()] - series1.loc[value_at_peak_idx]
+    return [value_at_peak,half_life_value]
+    
 df = pd.read_excel('data.xlsx')
 print(df)
 
